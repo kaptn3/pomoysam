d9f933d2e7beeac1503cb2396f375c1ecad79f7f
Victoria <kapitonenko.vick@ya.ru>
2020-05-14 23:49:03 +0300

    feat: methods for faq

---
+++ src/components/AddFaq.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="add-faq">
+    <b-message
+      v-if="status === 'ok'"
+      type="is-success"
+    >
+      Готово
+    </b-message>
+    <b-message
+      v-if="status === 'error'"
+      type="is-warning"
+    >
+      Произошла ошибка
+    </b-message>
+    <form
+      v-if="status === ''"
+      @submit="submitForm"
+    >
+      <h2 class="title is-3">
+        Добавить
+      </h2>
+      <b-field label="Вопрос">
+        <b-input
+          v-model="question"
+          required
+        />
+      </b-field>
+      <b-field label="Ответ">
+        <b-input
+          v-model="answer"
+          maxlength="200"
+          type="textarea"
+          required
+        />
+      </b-field>
+      <button class="button is-info is-fullwidth">
+        {{ btnText }}
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    name: 'AddFaq',
+    props: {
+      row: {
+        type: Object,
+        default: null
+      }
+    },
+    data() {
+      return {
+        status: '',
+        questionText: null,
+        answerText: null
+      };
+    },
+    computed: {
+      question: {
+        get() {
+          if (this.row) {
+            return this.row.question;
+          }
+          return '';
+        },
+        set(value) {
+          this.questionText = value;
+        }
+      },
+      answer: {
+        get() {
+          if (this.row) {
+            return this.row.answer;
+          }
+          return '';
+        },
+        set(value) {
+          this.answerText = value;
+        }
+      },
+      btnText() {
+        return this.row ? 'Сохранить' : 'Добавить';
+      },
+      url() {
+        return `${process.env.VUE_APP_API}admFaq/`;
+      }
+    },
+    methods: {
+      submitForm(e) {
+        e.preventDefault();
+        const data = new FormData();
+        data.set('answer', this.answerText);
+        data.set('question', this.questionText);
+        if (this.row) {
+          data.set('id', this.row.id);
+        }
+        const method = this.row ? 'put' : 'post';
+        axios({
+          method,
+          url: this.url,
+          data,
+          headers: {
+            Authorization: `Token ${this.$store.state.token}`
+          }
+        })
+          .then(() => {
+            this.status = 'ok';
+          })
+          .catch(() => {
+            this.status = 'error';
+          });
+      }
+    }
+  };
+</script>
+
+<style scoped>
+.add-faq {
+  background-color: white;
+  padding: 20px;
+}
+</style>
--- src/views/Faq.vue
+++ src/views/Faq.vue
@@ -1,24 +1,63 @@
 <template>
-  <b-table
-    :data="data"
-    :columns="head"
-    :loading="loading"
-    :hoverable="true"
-    :mobile-cards="true"
-  >
-    <template slot="empty">
-      <section class="content has-text-grey has-text-centered">
-        <p>Данные не найдены</p>
-      </section>
-    </template>
-  </b-table>
+  <section class="container">
+    <b-button
+      type="is-info"
+      @click="isModalActive = true"
+    >
+      Добавить
+    </b-button>
+    <b-table
+      :data="data"
+      :columns="head"
+      :loading="loading"
+      :hoverable="true"
+      :mobile-cards="true"
+      @click="editRow"
+    >
+      <template slot-scope="props">
+        <b-table-column
+          v-for="(value, name) in props.row"
+          :key="name"
+          :style="name === 'id' ? 'display: none' : ''"
+          :field="name"
+        >
+          {{ value }}
+        </b-table-column>
+        <b-table-column
+          field="actions"
+          label=" "
+        >
+          <b-button
+            type="is-danger"
+            @click="deleteFaq(props.row.id)"
+          >
+            Удалить
+          </b-button>
+        </b-table-column>
+      </template>
+      <template slot="empty">
+        <section class="content has-text-grey has-text-centered">
+          <p>Данные не найдены</p>
+        </section>
+      </template>
+    </b-table>
+    <b-modal
+      :active.sync="isModalActive"
+      :width="640"
+      scroll="keep"
+    >
+      <add-faq :row="currentRow" />
+    </b-modal>
+  </section>
 </template>
 
 <script>
   import axios from 'axios';
+  import AddFaq from '../components/AddFaq';
 
   export default {
     name: 'Faq',
+    components: { AddFaq },
     data() {
       return {
         data: [],
@@ -30,24 +69,68 @@
           {
             field: 'answer',
             label: 'Ответ'
+          },
+          {
+            field: 'actions',
+            label: ' '
           }
-        ]
+        ],
+        loading: true,
+        isModalActive: false,
+        currentRow: null,
+        isDelete: false
       };
+    },
+    computed: {
+      url() {
+        return `${process.env.VUE_APP_API}admFaq/`;
+      }
+    },
+    watch: {
+      isModalActive() {
+        if (!this.isModalActive) {
+          this.getFaq();
+          this.currentRow = null;
+        }
+      }
     },
     mounted() {
       this.getFaq();
     },
     methods: {
       getFaq() {
-        const url = `${process.env.VUE_APP_API}admFaq/`;
-        axios.get(url, this.$store.getters.config)
+        this.loading = true;
+        axios.get(this.url, this.$store.getters.config)
           .then((res) => {
             this.data = res.data;
+            this.loading = false;
           })
           .catch(() => {
             this.$router.push('/login');
           });
+      },
+      deleteFaq(row) {
+        this.isDelete = true;
+        const data = new FormData();
+        data.set('id', row);
+        axios.delete(this.url, {
+          data,
+          headers: {
+            Authorization: `Token ${this.$store.state.token}`
+          }
+        })
+          .then(() => {
+            this.getFaq();
+            this.isDelete = false;
+          })
+          .catch(() => {
+            this.$router.push('/login');
+          });
+      },
+      editRow(row) {
+        this.currentRow = row;
+        this.isModalActive = true && !this.isDelete;
       }
-    }
+    },
   };
 </script>
